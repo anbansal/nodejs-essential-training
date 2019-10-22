3f1dfe4ef4c00a1896256d8c62a36efd9941c4ea
Ankur <ankurbnsl8099@gmail.com>
2019-10-22 19:09:19 -0400

    adding the list, function, process.stdout.write in nodeJs

--- Chapter-03/03_01/standOut.js
+++ Chapter-03/03_01/standOut.js
@@ -1,2 +1,16 @@
-process.stdout.write("Hola ");
-process.stdout.write("world\n\n\n"); 
+const questions = [
+    "How are you",
+    "What did you do today",
+    "What is your favourite color",
+    "What is your hobby",
+    "Where are you coming from",
+    "Where are you going",
+    "What is your background"
+]
+
+const ask = (i = 0) => {
+    process.stdout.write(`\n${questions[i]}\n`);
+}
+
+
+ask(process.argv[2]);
